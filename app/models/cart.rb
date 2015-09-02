2f516c3ea5901dc0ae84eecf0ab0a9d5812dc46e
Myo T Kyaw <myotkyaw@gmail.com>
2015-09-02 13:56:44 -0700

    Add and implement Add to Cart button

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,5 +2,5 @@ class Cart < ActiveRecord::Base
 
   # "dependent :destroy" - existence of line-items is dependent on existence of cart
   # if cart is destroyed => Rails destroys line-items also
-  has_many :line_items, dependent :destroy
+  has_many :line_items, dependent: :destroy
 end
